98a4b6bdb020e0b530aa6192074ad26b61c37b67
Dasgar7 <dasgari769@gmail.com>
2026-07-31 00:18:37 +0400

    CSS: ensure start button clickable; SW cache v9

--- sw.js
+++ sw.js
@@ -1,34 +1,16 @@
-const CACHE = 'bio-explorer-v8';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './game.js',
-  './manifest.json',
-  './icons/icon.svg'
-];
-
+const CACHE = 'bio-explorer-v9';
+const ASSETS = ['./','./index.html','./style.css','./game.js','./manifest.json','./icons/icon.svg'];
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', (e) => {
   const url = new URL(e.request.url);
   if (url.origin.includes('unpkg.com') || url.origin.includes('cdn')) {
     e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
     return;
   }
-  e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
 });
