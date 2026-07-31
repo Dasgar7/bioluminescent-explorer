ed3f9e263650020d29021b8fc117fd0bfef23ce1
Dasgar7 <dasgari769@gmail.com>
2026-07-31 10:39:31 +0400

    RESTORE city: fixed blank scene - clean build, no Object.assign, shadows off, cache v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bio-explorer-v11';
+const CACHE = 'bio-explorer-v12';
 const ASSETS = ['./','./index.html','./style.css','./manifest.json','./icons/icon.svg'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -8,7 +8,6 @@ self.addEventListener('activate', (e) => {
 });
 self.addEventListener('fetch', (e) => {
   const url = new URL(e.request.url);
-  // Always network-first for game.js so code updates are never stuck in cache
   if (url.pathname.endsWith('game.js') || url.pathname.endsWith('sw.js')) {
     e.respondWith(fetch(e.request).then((res) => {
       const copy = res.clone();
