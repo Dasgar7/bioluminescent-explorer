4bbe90c7c3c313342af810466d2f487fbdfb2e10
Dasgar7 <dasgari769@gmail.com>
2026-08-01 15:09:13 +0400

    v26: bump service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'city-explorer-v25b';
+const CACHE = 'city-explorer-v26';
 const ASSETS = ['./', './index.html', './style.css', './manifest.json', './icons/icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
